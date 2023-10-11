71063adfecb3f90293d9d2154dd5bb76c415ec84
Tarun Raj <raj.tarun@icloud.com>
2023-10-11 12:02:50 +0530

    hosted on vercel

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -15,7 +15,7 @@ const Footer = () => {
   console.log(errors);
 
   return (
-    <footer className="mt-16 rounded-2xl bg-dark dark:bg-accentDark/90 m-2 sm:m-10 flex flex-col items-center text-light dark:text-dark">
+    <footer className="mt-16 rounded-2xl bg-light dark:bg-accentDark/90 m-2 sm:m-10 flex flex-col items-center text-dark dark:text-light">
       <h3 className="mt-16 font-medium dark:font-bold text-center capitalize text-2xl sm:text-3xl lg:text-4xl px-4">
         Interesting Stories | Updates | Guides
       </h3>
@@ -63,21 +63,13 @@ const Footer = () => {
           aria-label="Check my profile on Github"
           target="_blank"
         >
-          <GithubIcon className="fill-light dark:fill-dark  hover:scale-125 transition-all ease duration-200" />
-        </a>
-        <a
-          href={siteMetadata.dribbble}
-          className="inline-block w-6 h-6 mr-4"
-          aria-label="Check my profile on Dribbble"
-          target="_blank"
-        >
-          <DribbbleIcon className="hover:scale-125 transition-all ease duration-200" />
+          <GithubIcon className="fill-dark dark:fill-light  hover:scale-125 transition-all ease duration-200" />
         </a>
       </div>
 
       <div className="w-full  mt-16 md:mt-24 relative font-medium border-t border-solid border-light py-6 px-8 flex  flex-col md:flex-row items-center justify-between">
         <span className="text-center">
-          &copy;2023 CodeBucks. All rights reserved.
+          &copy;2023 pa1ncakes. All rights reserved.
         </span>
         <Link
           href="/sitemap.xml"
@@ -87,8 +79,12 @@ const Footer = () => {
         </Link>
         <div className="text-center">
           Made with &hearts; by{" "}
-          <a href="https://devdreaming.com" className="underline" target="_blank">
-            CodeBucks
+          <a
+            href="https://github.com/pa1ncakes"
+            className="underline"
+            target="_blank"
+          >
+            pa1ncakes
           </a>
         </div>
       </div>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,95 +1,202 @@
-"use client"
+"use client";
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import Logo from "./Logo";
-import { DribbbleIcon, GithubIcon, LinkedinIcon, MoonIcon, SunIcon, TwitterIcon } from "../Icons";
+import {
+  GithubIcon,
+  LinkedinIcon,
+  MoonIcon,
+  SunIcon,
+  TwitterIcon,
+} from "../Icons";
 import siteMetadata from "@/src/utils/siteMetaData";
 import { useThemeSwitch } from "../Hooks/useThemeSwitch";
-import { useState } from "react";
 import { cx } from "@/src/utils";
 
 const Header = () => {
-
   const [mode, setMode] = useThemeSwitch();
   const [click, setClick] = useState(false);
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true);
 
-const toggle = () =>{
-  setClick(!click)
-}
+  const toggle = () => {
+    setClick(!click);
+  };
+
+  useEffect(() => {
+    // Adding transition class to the body to handle theme transitions
+    document.body.classList.add(
+      "transition-colors",
+      "duration-700",
+      "ease-in-out",
+    );
+    // Optional: Remove transition classes after switching to avoid other unwanted transitions
+    const transitionRemover = setTimeout(() => {
+      document.body.classList.remove(
+        "transition-colors",
+        "duration-700",
+        "ease-in-out",
+      );
+    }, 500);
+
+    return () => clearTimeout(transitionRemover);
+  }, [mode]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+      const visible = prevScrollPos > currentScrollPos || currentScrollPos <= 0;
+      setPrevScrollPos(currentScrollPos);
+      setVisible(visible);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [prevScrollPos, setVisible]);
   return (
-    <header className="w-full p-4  px-5 sm:px-10 flex items-center justify-between">
-        <Logo />
+    <header
+      className={`w-full p-4 px-5 sm:px-10 flex items-center justify-between 
+        transition-transform ease-in-out duration-700 ${
+          visible ? "translate-y-0" : "-translate-y-full"
+        } 
+        transition-colors duration-700 ${
+          mode === "dark" ? "bg-dark text-light" : "bg-light text-dark"
+        }`}
+    >
+      <Logo />
 
-        <button className="inline-block sm:hidden z-50" onClick={toggle} aria-label="Hamburger Menu">
-          <div className="w-6 cursor-pointer transition-all ease duration-300">
-            <div className="relative">
-            <span className="absolute top-0 inline-block w-full h-0.5 bg-dark dark:bg-light rounded transition-all ease duration-200" 
-            style={{
-             transform: click ? "rotate(-45deg) translateY(0)" : "rotate(0deg) translateY(6px)"
-            }}
-            
-            >&nbsp;</span>
-            <span className="absolute top-0 inline-block w-full h-0.5 bg-dark dark:bg-light rounded transition-all ease duration-200"
-            style={{
-              opacity: click ? 0 : 1
-             }}
-            >&nbsp;</span>
-            <span className="absolute top-0 inline-block w-full h-0.5 bg-dark dark:bg-light rounded transition-all ease duration-200"
-            style={{
-              transform: click ? "rotate(45deg) translateY(0)" : "rotate(0deg) translateY(-6px)"
-             }}
-            >&nbsp;</span>
-            </div>
-
+      <button
+        className="inline-block sm:hidden z-50"
+        onClick={toggle}
+        aria-label="Hamburger Menu"
+      >
+        <div className="w-6 cursor-pointer transition-all ease duration-700">
+          <div className="relative">
+            <span
+              className="absolute top-0 inline-block w-full h-0.5 bg-dark dark:bg-light rounded transition-all ease duration-200"
+              style={{
+                transform: click
+                  ? "rotate(-45deg) translateY(0)"
+                  : "rotate(0deg) translateY(6px)",
+              }}
+            >
+              &nbsp;
+            </span>
+            <span
+              className="absolute top-0 inline-block w-full h-0.5 bg-dark dark:bg-light rounded transition-all ease duration-200"
+              style={{
+                opacity: click ? 0 : 1,
+              }}
+            >
+              &nbsp;
+            </span>
+            <span
+              className="absolute top-0 inline-block w-full h-0.5 bg-dark dark:bg-light rounded transition-all ease duration-200"
+              style={{
+                transform: click
+                  ? "rotate(45deg) translateY(0)"
+                  : "rotate(0deg) translateY(-6px)",
+              }}
+            >
+              &nbsp;
+            </span>
           </div>
-        </button>
+        </div>
+      </button>
 
-        <nav className=" w-max py-3 px-6 sm:px-8 border border-solid border-dark rounded-full font-medium capitalize  items-center flex  sm:hidden
+      <nav
+        className=" w-max py-3 px-6 sm:px-8 border border-solid border-dark rounded-full font-medium capitalize  items-center flex  sm:hidden
         fixed top-6 right-1/2 translate-x-1/2 bg-light/80 backdrop-blur-sm z-50
-        transition-all ease duration-300
+        transition-all ease duration-700
         "
         style={{
-          top: click ? "1rem" : "-5rem"
-         }}
-        
+          top: click ? "1rem" : "-5rem",
+        }}
+      >
+        <Link href="/" className="mr-2">
+          Home
+        </Link>
+        <Link href="/about" className="mx-2">
+          About
+        </Link>
+        <Link href="/contact" className="mx-2">
+          Contact
+        </Link>
+        <button
+          onClick={() => setMode(mode === "light" ? "dark" : "light")}
+          className={cx(
+            "w-6 h-6 ease ml-2 flex items-center justify-center rounded-full p-1",
+            mode === "light" ? "bg-dark text-light" : "bg-light text-dark",
+          )}
+          aria-label="theme-switcher"
         >
-            <Link href="/" className="mr-2">Home</Link>
-            <Link href="/about" className="mx-2">About</Link>
-            <Link href="/contact" className="mx-2">Contact</Link>
-            <button onClick={() => setMode(mode === "light" ? "dark" : "light")  }
-            className={cx("w-6 h-6 ease ml-2 flex items-center justify-center rounded-full p-1", mode === "light" ? "bg-dark text-light" :
-            "bg-light text-dark" )}
-            aria-label="theme-switcher"
-            >
-                {
-                  mode === "light" ? <MoonIcon className={"fill-dark"} />  : <SunIcon className={"fill-dark"} />
-                }
-            </button>
-        </nav>
+          {mode === "light" ? (
+            <MoonIcon className={"fill-dark"} />
+          ) : (
+            <SunIcon className={"fill-dark"} />
+          )}
+        </button>
+      </nav>
 
-
-        <nav className=" w-max py-3 px-8 border border-solid border-dark rounded-full font-medium capitalize  items-center hidden sm:flex
-        fixed top-6 right-1/2 translate-x-1/2 bg-light/80 backdrop-blur-sm z-50">
-            <Link href="/" className="mr-2">Home</Link>
-            <Link href="/about" className="mx-2">About</Link>
-            <Link href="/contact" className="mx-2">Contact</Link>
-            <button onClick={() => setMode(mode === "light" ? "dark" : "light")  }
-            className={cx("w-6 h-6 ease ml-2 flex items-center justify-center rounded-full p-1", mode === "light" ? "bg-dark text-light" :
-            "bg-light text-dark" )}
-            aria-label="theme-switcher"
-            >
-                {
-                  mode === "light" ? <MoonIcon className={"fill-dark"} />  : <SunIcon className={"fill-dark"} />
-                }
-            </button>
-        </nav>
-        <div className=" hidden sm:flex items-center">
-            <a href={siteMetadata.linkedin} className="inline-block w-6 h-6 mr-4" aria-label="Reach out to me via LinkedIn" target="_blank"><LinkedinIcon className="hover:scale-125 transition-all ease duration-200" /></a>
-            <a href={siteMetadata.twitter} className="inline-block w-6 h-6 mr-4" aria-label="Reach out to me via Twitter" target="_blank"><TwitterIcon className="hover:scale-125 transition-all ease duration-200" /></a>
-            <a href={siteMetadata.github} className="inline-block w-6 h-6 mr-4" aria-label="Check my profile on Github" target="_blank"><GithubIcon className="  hover:scale-125 transition-all ease duration-200 dark:fill-light" /></a>
-            <a href={siteMetadata.dribbble} className="inline-block w-6 h-6 mr-4" aria-label="Check my profile on Dribbble" target="_blank"><DribbbleIcon className="hover:scale-125 transition-all ease duration-200" /></a>
-        </div>
+      <nav
+        className=" w-max py-3 px-8 border border-solid border-dark rounded-full font-medium capitalize  items-center hidden sm:flex
+        fixed top-6 right-1/2 translate-x-1/2 bg-light/80 backdrop-blur-sm z-50"
+      >
+        <Link href="/" className="mr-2">
+          Home
+        </Link>
+        <Link href="/about" className="mx-2">
+          About
+        </Link>
+        <Link href="/contact" className="mx-2">
+          Contact
+        </Link>
+        <button
+          onClick={() => setMode(mode === "light" ? "dark" : "light")}
+          className={cx(
+            "w-6 h-6 ease ml-2 flex items-center justify-center rounded-full p-1",
+            mode === "light" ? "bg-dark text-light" : "bg-light text-dark",
+          )}
+          aria-label="theme-switcher"
+        >
+          {mode === "light" ? (
+            <MoonIcon className={"fill-dark"} />
+          ) : (
+            <SunIcon className={"fill-dark"} />
+          )}
+        </button>
+      </nav>
+      <div className=" hidden sm:flex items-center">
+        <a
+          href={siteMetadata.linkedin}
+          className="inline-block w-6 h-6 mr-4"
+          aria-label="Reach out to me via LinkedIn"
+          target="_blank"
+        >
+          <LinkedinIcon className="hover:scale-125 transition-all ease duration-300" />
+        </a>
+        <a
+          href={siteMetadata.twitter}
+          className="inline-block w-6 h-6 mr-4"
+          aria-label="Reach out to me via Twitter"
+          target="_blank"
+        >
+          <TwitterIcon className="hover:scale-125 transition-all ease duration-300" />
+        </a>
+        <a
+          href={siteMetadata.github}
+          className="inline-block w-6 h-6 mr-4"
+          aria-label="Check my profile on Github"
+          target="_blank"
+        >
+          <GithubIcon className="  hover:scale-125 transition-all ease duration-300 dark:fill-light" />
+        </a>
+      </div>
     </header>
-  )
-}
+  );
+};
 
 export default Header;
